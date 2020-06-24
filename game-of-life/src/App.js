9764e7641ee5131ec907d452444d63b77ff4eea4
Mario Rivera <mario@Marios-MacBook-Pro.local>
2020-06-24 11:48:58 -0700

    set up grid, installed immer

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import produce from 'immer';
+// import { set } from 'immer/dist/internal';
+
+const numRows = 50;
+const numCols = 50;
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+    return rows;
+  });
+  // console.log(grid);
+
+  const [running, setRunning] = useState(false);
+
+
   return (
-    <div>
-      <h3>Game of Life</h3>
-    </div>
+    <>
+        <button 
+          onClick={() => {
+            setRunning(!running);
+          }}
+          >
+            {running ? 'Stop' : 'Start!'}
+          </button>
+        <div style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}>
+          {grid.map((rows, i) => 
+            rows.map((col, k) => <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                })
+                setGrid(newGrid);
+              }}
+              style={{ 
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? 'blue' : undefined,
+                border: 'solid 1px black'
+                }}/>))}
+        </div>
+    </>
   );
 }
 
